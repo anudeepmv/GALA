87f75889b7e7d99d4a6fe95552320642abdb1a6f
anudeepmv <medam.2@wright.edu>
2022-04-27 17:55:10 -0400

    formatted documentation and literature programming for add,update,delete

--- controller/user/index.js
+++ controller/user/index.js
@@ -1,3 +1,6 @@
+///https://github.com/anudeepmv/WEBSTER/issues/13
+///https://github.com/anudeepmv/WEBSTER/issues/16
+///This mode is part of index subsystem.javascript is described at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf/
 const user =require('../../service/user');
 
 // Get info of single user
@@ -5,19 +8,19 @@ const get=(req,res)=>{
     res.status(200).send(user.getOne(req.params.id))
 }
 
-
+/// The function add is to add the user
 const add=(req,res)=>{
     user.addUser(req.body).then((result)=>{
         res.status(200).send({ success:true, data:result, message:"User successfully added" });
     }).catch(err=>{res.status(500).send({ success:false, message:err });})
 }
-
+/// The function update is to update the user
 const update=(req,res)=>{
     user.update(req.params.id,req.body).then((result)=>{
         res.status(200).send({ success:true, data:result, message:"User successfully added" });
     }).catch(err=>{res.status(500).send({ success:false, message:err });})
 }
-
+/// The Function delete is to delete the user
 const _delete=(req,res)=>{
     user.delete(req.params.id).then((result)=>{
         res.status(200).send({ success:true, data:result, message:"User successfully added" });
